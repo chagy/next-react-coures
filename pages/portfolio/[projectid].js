dc07b78413a62bfb78a632a29d1adf20f0587f6c
chagy <chagy_x_@hotmail.com>
2021-05-01 13:47:56 +0700

    adding dynamic paths & routes

---
+++ pages/portfolio/[projectid].js
@@ -0,0 +1,9 @@
+function PortfolioProjectPage() {
+  return (
+    <div>
+      <h1>Project Page</h1>
+    </div>
+  );
+}
+
+export default PortfolioProjectPage;
---
+++ pages/portfolio/index.js
@@ -0,0 +1,9 @@
+function PorfolioPage() {
+  return (
+    <div>
+      <h1>PorfolioPage</h1>
+    </div>
+  );
+}
+
+export default PorfolioPage;
