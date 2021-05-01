c70fcae78a4979e9498dad70a99fe36a7353f79a
chagy <chagy_x_@hotmail.com>
2021-05-01 13:53:29 +0700

    Extracting Dynamic

--- pages/portfolio/[projectid].js
+++ pages/portfolio/[projectid].js
@@ -1,4 +1,10 @@
+import { useRouter } from "next/router";
 function PortfolioProjectPage() {
+  const router = useRouter();
+
+  console.log(router.pathname);
+  console.log(router.query);
+
   return (
     <div>
       <h1>Project Page</h1>
