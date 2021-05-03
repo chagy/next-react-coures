c4e29c36df5044150ed891165b13671b108a42ab
chagy <chagy_x_@hotmail.com>
2021-05-03 15:03:48 +0700

    Adding Backend code

--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,58 @@
-import { getFeaturedEvents } from "../helpers/api-util";
-import EventList from "../components/events/event-list";
-
-import Head from "next/head";
-import Link from "next/link";
+import { useRef, useState } from "react";
 
 function HomePage(props) {
+  const [feedbackItems, setFeedbackItems] = useState([]);
+  const emailInputRef = useRef();
+  const feedbackInputRef = useRef();
+  function submitFormHandler(event) {
+    event.preventDefault();
+
+    const enteredEmail = emailInputRef.current.value;
+    const enteredFeedback = feedbackInputRef.current.value;
+
+    const reqBody = { email: enteredEmail, text: enteredFeedback };
+
+    fetch(`/api/feedback`, {
+      method: "POST",
+      body: JSON.stringify(reqBody),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  }
+
+  function loadFeedbackHandler() {
+    fetch(`/api/feedback`)
+      .then((response) => response.json())
+      .then((data) => {
+        setFeedbackItems(data.feedback);
+      });
+  }
   return (
     <div>
-      <Head>
-        <title>NextJS Events</title>
-        <meta name="description" content="test meta" />
-      </Head>
-      <EventList items={props.events} />
+      <h1>Home PAge</h1>
+      <form onSubmit={submitFormHandler}>
+        <div>
+          <label htmlFor="email">Your Email</label>
+          <input type="email" id="email" ref={emailInputRef} />
+        </div>
+        <div>
+          <label htmlFor="feedback">Your Feedback</label>
+          <textarea id="feedback" rows="5" ref={feedbackInputRef}></textarea>
+        </div>
+        <button>Send Feedback</button>
+      </form>
+      <hr />
+      <button onClick={loadFeedbackHandler}>Load Feedback</button>
+      <ul>
+        {feedbackItems.map((item) => (
+          <li key={item.id}>{item.text}</li>
+        ))}
+      </ul>
     </div>
   );
-}
-
-export async function getStaticProps() {
-  const featuredEvents = await getFeaturedEvents();
-  return {
-    props: {
-      events: featuredEvents,
-    },
-    revalidate: 1800,
-  };
 }
 
 export default HomePage;
